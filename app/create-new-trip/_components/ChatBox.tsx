3f04d741226abd675936f74bf2e6d0938ef5360c
Akbaree <turahojaevakbarhoja@gmail.com>
2026-03-05 13:22:13 -0700

    Initial commit - AI Trip Planner

---
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -0,0 +1,121 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import axios from 'axios'
+import { Loader, Send } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import EmptyBoxState from './EmptyBoxState'
+import GroupSizeUI from './GroupSizeUI'
+import BudgetUi from './BudgetUi'
+
+type Message = {
+    role:string,
+    content:string,
+    ui?:string,
+}
+
+function ChatBox() {
+    
+    const [messages, setMessages]=useState<Message[]>([]);
+    const [userInput,setUserInput]=useState<string>();
+    const [loading,setLoading]=useState(false);
+    const [isFinal,setIsFinal]=useState(false);
+    const onsend=async()=>{
+
+
+        if (!userInput?.trim()) return;
+        setLoading(true);
+        setUserInput('');
+        const newMsg:Message={
+            role:'user',
+            content:userInput ?? ''
+        }
+
+        setMessages((prev:Message[])=>[...prev,newMsg]);
+
+        const result = await axios.post('/api/aimodel',{
+            messages:[...messages,newMsg],
+            isFinal:isFinal
+        });
+
+        console.log("TRIP",result.data);
+
+        !isFinal && setMessages((prev:Message[])=>[...prev,{
+            role:'assistant',
+            content:result?.data?.resp,
+            ui:result?.data?.ui
+        }])
+  
+        setLoading(false);
+    }
+
+    const RenderGenerativeUi = (ui:string)=>{
+        if (ui == 'budget'){
+            return <BudgetUi onSelectedOption={(v:string)=>{setUserInput(v); onsend()}}/>
+        } else if (ui == 'groupSize') {
+            return <GroupSizeUI onSelectedOption={(v:string)=>{setUserInput(v); onsend()}} />
+        }
+
+        return null
+    }
+
+    useEffect(()=>{
+        const lastMsg = messages[messages.length-1];
+        if(lastMsg?.ui=='final')
+        {
+            setIsFinal(true);
+            setUserInput('Ok, Great!')
+            onsend();
+        }
+    }, [messages])
+
+
+
+  return (
+    <div className='h-[85vh] flex flex-col'>
+        {messages?.length == 0 &&
+            <EmptyBoxState onSelectOption={(v:string)=>{ setUserInput(v); onsend()}}/>
+        }
+        <section className='flex-1 overflow-y-auto p-4'>
+            {messages.map((msg:Message, index)=>(
+                msg.role=='user'?
+            <div className='flex justify-end mt-2' key={index}>
+                <div className='max-w-lg bg-primary text-white px-4 py-2 rounded-lg'>
+                    {msg.content}
+                </div>
+            </div>:
+
+            <div className='flex justify-start mt-2' key={index}>
+                <div className='max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg'>
+                    {msg.content}
+                    {RenderGenerativeUi(msg.ui??'')}
+                </div>
+            </div>
+
+            ))}
+            
+            {loading && <div className='flex justify-start mt-2' >
+                <div className='max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg'>
+                    {<Loader className='animate-spin'/>}
+                </div>
+            </div>}
+
+        </section>
+
+        <section>
+            <div className='border rounded-2xl p-4 shadow relative'>
+                    <Textarea placeholder = 'Start typing here...' 
+                    className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none'
+                    onChange={(event)=>setUserInput(event.target.value??'')}
+                    value ={userInput}
+                    />
+                    <Button size={'icon'} className='absolute bottom-6 right-6' onClick={()=>onsend()}>
+                        <Send className='h-4 w-4'/>
+                    </Button>
+                </div>
+        </section>
+    </div>
+  )
+}
+
+export default ChatBox
